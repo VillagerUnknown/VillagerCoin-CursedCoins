721ad1e7bdf6be0a109357870ae8881b208c30d4
villagerunknown <VillagerUnknown@proton.me>
2025-05-12 01:09:14 -0400

    Cleaning up

--- src/main/java/me/villagerunknown/cursedcoins/Cursedcoins.java
+++ src/main/java/me/villagerunknown/cursedcoins/Cursedcoins.java
@@ -1,30 +1,32 @@
 package me.villagerunknown.cursedcoins;
 
-import me.villagerunknown.cursedcoins.loader.InventoryEffectCoinFeatureLoader;
+import me.villagerunknown.cursedcoins.feature.CursedCoinItemsFeature;
 import me.villagerunknown.platform.Platform;
 import me.villagerunknown.platform.PlatformMod;
 import me.villagerunknown.platform.manager.featureManager;
+import me.villagerunknown.villagercoin.Villagercoin;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 
 public class Cursedcoins implements ModInitializer {
 	
-	public static PlatformMod<CursedcoinsConfigData> MOD = Platform.register("cursedcoins", Cursedcoins.class, CursedcoinsConfigData.class );
+	public static PlatformMod<?> MOD = Platform.register("cursedcoins", Cursedcoins.class );
 	public static String MOD_ID = MOD.getModId();
 	public static Logger LOGGER = MOD.getLogger();
-	public static CursedcoinsConfigData CONFIG = MOD.getConfig();
 	
 	@Override
 	public void onInitialize() {
-		// # Initialize Mod
-		init();
-	}
-	
-	private static void init() {
+		// # Load Villager Coin
+		Villagercoin.load();
+		
+		// # Initialize addon mod with Platform
 		Platform.init_mod( MOD );
 		
 		// # Activate Features
-		featureManager.addFeature( "inventoryEffectCoinFeatureLoader", InventoryEffectCoinFeatureLoader::execute );
+		featureManager.addFeature( "cursed-coin-items", CursedCoinItemsFeature::execute );
+		
+		// # Load Features
+		featureManager.loadFeatures();
 	}
 	
 }
--- src/main/java/me/villagerunknown/cursedcoins/feature/CursedCoinItemsFeature.java
+++ src/main/java/me/villagerunknown/cursedcoins/feature/CursedCoinItemsFeature.java
@@ -1,9 +1,9 @@
-package me.villagerunknown.cursedcoins.loader;
+package me.villagerunknown.cursedcoins.feature;
 
 import me.villagerunknown.cursedcoins.item.CursedCoinItems;
 import me.villagerunknown.villagercoin.feature.InventoryEffectCoinFeature;
 
-public class InventoryEffectCoinFeatureLoader {
+public class CursedCoinItemsFeature {
 	
 	public static void execute() {
 		InventoryEffectCoinFeature.execute();
